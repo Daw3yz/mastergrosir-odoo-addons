47fca940a2eb022d9ab752ccd870f9c3df7bfea5
David <davidangka@gmail.com>
2024-03-04 14:23:18 +0700

    Initial commit

---
+++ avi_customer_table/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Mastergrosir Custom Customer Table',
+    'author': 'Avisoft System Solution',
+    'version': '17.0.1.2.7',
+    'category': '',
+    'depends': [
+        'base',
+        'account',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'installable': True
+}
